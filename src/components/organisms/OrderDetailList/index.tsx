aaa8360d2620af0debbd177355dccda04b738a93
saumitra403 <mishra.saumitra06@gmail.com>
2022-08-03 09:44:01 +0530

    Added Logic

--- src/components/organisms/OrderDetailList/index.tsx
+++ src/components/organisms/OrderDetailList/index.tsx
@@ -1,7 +1,12 @@
 import { Box } from "@mui/material"
+import { useEffect } from "react";
 
 
 const OrderList = ({data} : any) => {
+    useEffect(() => {
+        //api call for adding elemeent to List
+        //api call for deleting element from the list
+    },data)
     return <Box>
         {/* map the data */}
     </Box>
